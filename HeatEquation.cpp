0a6027acf57f68a234cdadfff930b22972c4bfbe
Alexander Chebotin <61747868+axothy@users.noreply.github.com>
2021-09-28 21:11:50 +0300

    Add files via upload

--- HeatEquation.cpp
+++ HeatEquation.cpp
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <cmath>
 #include <vector>
+#include <array>
+#include "C:\Users\globa\source\repos\Project10\Project10\freeglut\include\GL\glut.h"
 
 #define N 7
 #define K 11
@@ -28,16 +30,16 @@ struct MaterialsData {
 };
 
 namespace MaterialDataBase {
-	std::vector<MaterialsData> materials = { 
+	std::vector<MaterialsData> materials = {
 		{490, 15, 7800},
-		{452, 92, 7870}, 
-		{128.7, 320, 19300}, 
+		{452, 92, 7870},
+		{128.7, 320, 19300},
 		{378, 100, 8600},
-		{383, 401, 8900 }, 
-		{139, 8.3, 13540}, 
-		{235, 430, 10500}, 
-		{502, 1700, 3500}, 
-		{500, 1, 25}, 
+		{383, 401, 8900 },
+		{139, 8.3, 13540},
+		{235, 430, 10500},
+		{502, 1700, 3500},
+		{500, 1, 25},
 		{0,0,0},
 		{1,1,1}
 	};
@@ -58,34 +60,39 @@ public:
 	double LeftBoundaryConditionFunction(double t);
 	double RightBoundaryConditionFunction(double t);
 
-	void SetCoordinateXGrid(double* x, const IntegrateParameters& params);
-	void SetTimeGrid(double* t, const IntegrateParameters& params);
+	void SetCoordinateXGrid(std::array<double, N>& x, const IntegrateParameters& params);
+	void SetTimeGrid(std::array<double, K>& t, const IntegrateParameters& params);
 
-	void getSolutionExplicit(const MaterialsData& material, double(*T)[N], double* x, double* t, const IntegrateParameters& params);
-	void getSolutionImplicit(const MaterialsData& material, double(*T)[N], double* x, double* t, const IntegrateParameters& params);
+	void getSolutionExplicit(const MaterialsData& material, double(*Temperature)[N], std::array<double, N>& x, std::array<double, K>& t,
+		const IntegrateParameters& params);
+	void getSolutionImplicit(const MaterialsData& material, double(*Temperature)[N], std::array<double, N>& x, std::array<double, K>& t,
+		const IntegrateParameters& params);
 };
 
 
 //НУ T(x,0) 
 double HeatEquationSolver::InitialValueFunction(double x)
 {
-	return 0.9 + 2 * x * (1 - x);
+	//return 0.9 + 2 * x * (1 - x);
+	return 0;
 }
 
 //ГУ1 T(0, t)
 double HeatEquationSolver::LeftBoundaryConditionFunction(double t)
 {
-	return 3 * (0.3 - 2 * t);
+	//return 3 * (0.3 - 2 * t);
+	return 20;
 }
 
 //ГУ2 T(l,t)
 double HeatEquationSolver::RightBoundaryConditionFunction(double t)
 {
-	return 1.38;
+	//return 1.38;
+	return 20;
 }
 
 //Создание сетки по координате х
-void HeatEquationSolver::SetCoordinateXGrid(double* x, const IntegrateParameters& params)
+void HeatEquationSolver::SetCoordinateXGrid(std::array<double, N>& x, const IntegrateParameters& params)
 {
 	x[0] = 0;
 	for (int i = 1; i < N; ++i) {
@@ -94,7 +101,7 @@ void HeatEquationSolver::SetCoordinateXGrid(double* x, const IntegrateParameters
 }
 
 //Создание сетки по времени 
-void HeatEquationSolver::SetTimeGrid(double* t, const IntegrateParameters& params)
+void HeatEquationSolver::SetTimeGrid(std::array<double, K>& t, const IntegrateParameters& params)
 {
 	t[0] = 0;
 	for (int k = 1; k < K; ++k) {
@@ -102,19 +109,20 @@ void HeatEquationSolver::SetTimeGrid(double* t, const IntegrateParameters& param
 	}
 }
 
-void HeatEquationSolver::getSolutionExplicit(const MaterialsData& material, double (*T)[N], double* x, double* t, const IntegrateParameters& params)
+void HeatEquationSolver::getSolutionExplicit(const MaterialsData& material, double (*Temperature)[N], std::array<double, N>& x,
+	std::array<double, K>& t, const IntegrateParameters& params)
 {
 	/*Задали начальные условия*/
 	for (int i = 0; i < N; ++i)
 	{
-		T[0][i] = InitialValueFunction(x[i]);
+		Temperature[0][i] = InitialValueFunction(x[i]);
 	}
 
 	/*Задали краевые условия*/
 	for (int k = 0; k < K; ++k)
 	{
-		T[k][0] = LeftBoundaryConditionFunction(t[k]);
-		T[k][N] = RightBoundaryConditionFunction(t[k]);
+		Temperature[k][0] = LeftBoundaryConditionFunction(t[k]);
+		Temperature[k][N] = RightBoundaryConditionFunction(t[k]);
 	}
 
 	/*Для сокращения выражений*/
@@ -130,25 +138,27 @@ void HeatEquationSolver::getSolutionExplicit(const MaterialsData& material, doub
 	{
 		for (int i = 1; i < N - 1; ++i)
 		{
-			T[k + 1][i] = (h * h * c_v * T[k][i] + delta_t * lambda * (T[k][i + 1] - 2 * T[k][i] + T[k][i - 1])) / (rho * c_v * h * h);
+			Temperature[k + 1][i] = (h * h * c_v * Temperature[k][i] + delta_t * lambda *
+				(Temperature[k][i + 1] - 2 * Temperature[k][i] + Temperature[k][i - 1])) / (rho * c_v * h * h);
 			//T[k + 1][i] = (delta_t / (h*h)) * (T[k][ i + 1] - 2 *T[k][ i] + T[k][ i - 1]) + T[k][ i];
 		}
 	}
 }
 
-void HeatEquationSolver::getSolutionImplicit(const MaterialsData& material, double(*T)[N], double* x, double* t, const IntegrateParameters& params)
+void HeatEquationSolver::getSolutionImplicit(const MaterialsData& material, double(*Temperature)[N], std::array<double, N>& x,
+	std::array<double, K>& t, const IntegrateParameters& params)
 {
 	/*Задали начальные условия*/
 	for (int i = 0; i < N; ++i)
 	{
-		T[0][i] = InitialValueFunction(x[i]);
+		Temperature[0][i] = InitialValueFunction(x[i]);
 	}
 
 	/*Задали краевые условия*/
 	for (int k = 0; k < K; ++k)
 	{
-		T[k][0] = LeftBoundaryConditionFunction(t[k]);
-		T[k][N] = RightBoundaryConditionFunction(t[k]);
+		Temperature[k][0] = LeftBoundaryConditionFunction(t[k]);
+		Temperature[k][N] = RightBoundaryConditionFunction(t[k]);
 	}
 
 	double c_v = material.cv_HeatCapacity;
@@ -168,7 +178,7 @@ void HeatEquationSolver::getSolutionImplicit(const MaterialsData& material, doub
 
 		for (int i = 0; i < N; i++)
 		{
-			F[i] = T[k][i] * ((rho * c_v) / (delta_t));
+			F[i] = Temperature[k][i] * ((rho * c_v) / (delta_t));
 		}
 		P[0] = C / B;
 		Q[0] = F[0] / B;
@@ -181,7 +191,7 @@ void HeatEquationSolver::getSolutionImplicit(const MaterialsData& material, doub
 
 		for (unsigned int j = N - 2; j > 0; --j)
 		{
-			T[k + 1][j] = P[j] * T[k + 1][j + 1] + Q[j];
+			Temperature[k + 1][j] = P[j] * Temperature[k + 1][j + 1] + Q[j];
 		}
 	}
 
@@ -199,13 +209,15 @@ int main()
 	params.h_xStep = 0.1;
 	params.delta_tStep = 0.01;
 
-	double T[K][N] = { 0 }; double x[N] = { 0 }; double t[K] = { 0 };
+	double Temperature[K][N] = { 0 };
+	std::array<double, N> x;
+	std::array<double, K> t;
 
 	mySolver.SetCoordinateXGrid(x, params);
 	mySolver.SetTimeGrid(t, params);
 
 	/*Здесь можно задать материал (см. enum)*/
-	mySolver.getSolutionImplicit(materials[Ones], T, x, t, params);
+	mySolver.getSolutionImplicit(materials[Ones], Temperature, x, t, params);
 
 	for (int i = 0; i < N; i++)
 		printf("%lf ", x[i]);
@@ -217,7 +229,7 @@ int main()
 	for (int i = 0; i < K; ++i)
 	{
 		for (int j = 0; j < N; ++j)
-			printf("%lf\t", T[i][j]);
+			printf("%lf\t", Temperature[i][j]);
 		printf("\n\n");
 	}
 
